d123104abfd7667513ba3a61842949561ce82cca
Jarvis <418536538@qq.com>
2021-04-28 23:32:03 +0800

    chore: 初始化项目

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: 'eslint:recommended',
+    root: true,
+    env: {
+        node: true,
+        es2021: true,
+        browser: true,
+    },
+};
---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    printWidth: 100,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    jsxSingleQuote: true,
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: 'avoid',
+    vueIndentScriptAndStyle: false,
+    endOfLine: 'lf',
+};
